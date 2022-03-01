db21be705d9a3378714fc3cecedbd0500a911e4c
Adetunji Samuel <samadetunji01@gmail.com>
2022-03-01 19:07:20 +0100

    Automatic configuration with django-heroku

--- notreddit/settings.py
+++ notreddit/settings.py
@@ -140,3 +140,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import django_heroku
+django_heroku.settings(locals())
